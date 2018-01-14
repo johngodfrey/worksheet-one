e86a94b64adc8879796432d8347b765761a560fc
johngodfrey <jgodfr04@dcs.bbk.ac.uk>
2018-01-14 21:08:20 +0000

    Ex06 passes all tests with Int values, Longs to be implemented.

--- exercise-six/src/PrimeFactorCalculator.kt
+++ exercise-six/src/PrimeFactorCalculator.kt
@@ -1,8 +1,18 @@
 object PrimeFactorCalculator {
 
     fun primeFactors(int: Int): List<Int> {
-        // TODO
-        return listOf<Int>()
+        var divisor = 2
+        var numerator = int
+        val primeFactors = mutableListOf<Int>()
+        while (numerator != 1) {
+            if (numerator % divisor == 0) {
+                numerator = numerator / divisor
+                primeFactors.add(divisor)
+            } else {
+                divisor++
+            }
+        }
+        return primeFactors
     }
 
     fun primeFactors(long: Long): List<Long> {
