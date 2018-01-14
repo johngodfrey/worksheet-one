bbfed69dab42e43a0eb89207e06b2a0d3b313320
johngodfrey <jgodfr04@dcs.bbk.ac.uk>
2018-01-14 17:05:25 +0000

    Ex02 passes all tests.

--- exercise-two/src/WordCount.kt
+++ exercise-two/src/WordCount.kt
@@ -1,7 +1,28 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        return HashMap<String, Int>()
+        val words = phrase.split(" ", ",\n", ",")
+        val trimmedWords = mutableListOf<String>()
+        for (word in words) {
+            var notQuoted = word
+            if (notQuoted.startsWith("'") && notQuoted.endsWith("'")) {
+                notQuoted = notQuoted.substring(1, notQuoted.length - 1)
+            }
+            val filtered = filter(notQuoted)
+            if (filtered.isNotEmpty()) {
+                trimmedWords.add(filtered)
+            }
+        }
+        return trimmedWords.groupingBy { it }.eachCount()
     }
 
+    fun filter(word: String): String {
+        val sb = StringBuilder()
+        for (ch in word) {
+            if (ch.isLetterOrDigit() || ch.toInt() == 39) {
+                sb.append(ch)
+            }
+        }
+        return sb.toString().toLowerCase()
+    }
 }
