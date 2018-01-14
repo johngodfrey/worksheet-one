c377d199697f64a5c14ca699b278bd5b44c40d72
johngodfrey <jgodfr04@dcs.bbk.ac.uk>
2018-01-14 19:50:04 +0000

    Ex04 passes all tests.

--- exercise-four/src/Anagram.kt
+++ exercise-four/src/Anagram.kt
@@ -1,6 +1,24 @@
 class Anagram(private val word: String) {
 
     fun match(anagrams: Collection<String>): Set<String> {
-        return setOf<String>()
+        val results = mutableSetOf<String>()
+        for (anagram in anagrams) {
+            if (word.length == anagram.length && word.toLowerCase() != anagram.toLowerCase()) {
+                val wordList = mutableListOf<Char>()
+                for (ch in word.toLowerCase()) {
+                    wordList.add(ch)
+                }
+                val anagramList = mutableListOf<Char>()
+                for (ch in anagram.toLowerCase()) {
+                    anagramList.add(ch)
+                }
+                wordList.sort()
+                anagramList.sort()
+                if (wordList == anagramList) {
+                    results.add(anagram)
+                }
+            }
+        }
+        return results
     }
 }
