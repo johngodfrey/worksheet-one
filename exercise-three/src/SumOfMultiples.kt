0c11584971eab6489c940c2f72f7ea2070662b36
johngodfrey <jgodfr04@dcs.bbk.ac.uk>
2018-01-14 17:51:00 +0000

    Ex03 passes all tests.

--- exercise-three/src/SumOfMultiples.kt
+++ exercise-three/src/SumOfMultiples.kt
@@ -1,4 +1,18 @@
 object SumOfMultiples {
 
-    fun sum(factors: Set<Int>, limit: Int): Int = 0
+    fun sum(factors: Set<Int>, limit: Int): Int {
+        val sums = mutableSetOf<Int>()
+        for (factor in factors) {
+            var counter = 0
+            while (counter * factor < limit) {
+                sums.add(counter * factor)
+                counter++
+            }
+        }
+        var total = 0
+        for (sum in sums) {
+            total += sum
+        }
+        return total
+    }
 }
