123cf11f354bffdd6d9c1c182bdc620f86b51f32
johngodfrey <jgodfr04@dcs.bbk.ac.uk>
2018-01-14 20:48:53 +0000

    Ex05 passes all tests.

--- exercise-five/src/Hexadecimal.kt
+++ exercise-five/src/Hexadecimal.kt
@@ -1,7 +1,25 @@
 object Hexadecimal {
 
     fun toDecimal(s: String): Int {
-        // TODO
-        return 0
+        var result = 0.0
+        val reversedInput = s.toLowerCase().toCharArray().reversed()
+        println(reversedInput)
+        var foundIllegalInput = false
+        val legalChars = listOf('a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+        for ((index, ch) in reversedInput.withIndex()) {
+            if (ch !in legalChars) {
+                foundIllegalInput = true
+            } else {
+                if (ch.isDigit()) {
+                    result += Integer.parseInt(ch.toString()) * Math.pow(16.0, index.toDouble())
+                } else {
+                    result += (ch.toInt() - 87) * Math.pow(16.0, index.toDouble())
+                }
+            }
+        }
+        if (foundIllegalInput) {
+            result = 0.0
+        }
+        return result.toInt()
     }
 }
