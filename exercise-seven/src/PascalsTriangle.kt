d60af68ccfcfd1892fdbb402823cbb3e321ab852
johngodfrey <jgodfr04@dcs.bbk.ac.uk>
2018-01-15 13:30:26 +0000

    Ex07 passes all tests.

--- exercise-seven/src/PascalsTriangle.kt
+++ exercise-seven/src/PascalsTriangle.kt
@@ -1,7 +1,23 @@
 object PascalsTriangle {
 
     fun computeTriangle(rows: Int): List<List<Int>> {
-        // TODO
-        return listOf(listOf<Int>())
+        if (rows < 0) {
+            throw IllegalArgumentException("Rows can't be negative!")
+        }
+        val result = mutableListOf<List<Int>>()
+        if (rows != 0) {
+            for (line in 0..rows - 1) {
+                val lineList = mutableListOf<Int>()
+                for (position in 0..line) {
+                    if (line == position || position == 0) {
+                        lineList.add(1)
+                    } else {
+                        lineList.add(result[line - 1][position - 1] + result[line - 1][position])
+                    }
+                }
+                result.add(lineList)
+            }
+        }
+        return result
     }
 }
